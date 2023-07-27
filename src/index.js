48a8df564db8ecf06fd9140110ae52b180165c0f
kkeian <kaserman22@gmail.com>
2023-07-26 21:40:59 -0400

    Add bootstrap CSS import to work w/react-bootstr

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Basic CSS file to use bootstrap with this application
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
