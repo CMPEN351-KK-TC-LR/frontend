c77908643bc647054e71ac933d8c0e549f0fda1b
kkeian <kaserman22@gmail.com>
2023-08-10 19:04:38 -0400

    Use NavLink instead

--- src/CustomNavbar.js
+++ src/CustomNavbar.js
@@ -1,5 +1,6 @@
 import logo from './PennStateSoft60x60.jpg';
 import Navbar from 'react-bootstrap/Navbar';
+import NavLink from 'react-bootstrap/NavLink';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import useAuth from './useAuth';
@@ -22,14 +23,14 @@ const CustomNavbar = () => {
                 <Nav className={currentUser ? 'me-auto' : 'ms-auto' }>
                     {currentUser ? (
                         <>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/profile">Profile</Nav.Link>
-                        <Nav.Link href='/logout'>Logout</Nav.Link>
+                        <NavLink href="/">Home</NavLink>
+                        <NavLink href="/profile">Profile</NavLink>
+                        <NavLink href='/logout'>Logout</NavLink>
                         </>
                     ) : (
                         <>
-                        <Nav.Link className='ms-auto' href="/login">Login</Nav.Link>
-                        <Nav.Link className='ms-auto' href='/register'>Register</Nav.Link>
+                        <NavLink className='ms-auto' href="/login">Login</NavLink>
+                        <NavLink className='ms-auto' href='/register'>Register</NavLink>
                         </>
                     )}
                 </Nav>
