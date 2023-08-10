83953b58ce1ddab682ee91cf76cb271b94266258
leo45rod <Leo45rodriguez@hotmail.com>
2023-08-10 14:34:15 -0400

    Menus

--- src/AdminMainMenu.js
+++ src/AdminMainMenu.js
@@ -3,11 +3,31 @@ import { Button, Container, Row, Col } from 'react-bootstrap';
 
 const AdmiMainMenu = () => {
     return (
-        <div>
-            <Link to='/admin-complaints'>
-                <Button>View Complaints</Button>
-            </Link>
-        </div>
+        <Container className="mt-5">
+            <Row>
+                <Col className="text-center">
+                    <h1>Admin Main Menu</h1>
+                </Col>
+            </Row>
+
+            <Row className="mt-4">
+                <Col md={{ span: 6, offset: 3 }}>
+                    <p>
+                        Click the button below to view all scheduled meetings.
+                    </p>
+                    <Link to='/admin-meetings'>
+                        <Button block>See Meetings</Button>
+                    </Link>
+
+                    <p>
+                        Click the button below to view all complaints that have been filed
+                    </p>
+                    <Link to='/admin-complaints'>
+                        <Button className="mb-3" block>View Complaints</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
     );
 }
  
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,9 +1,33 @@
+import { Link } from 'react-router-dom';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+
 const MainMenu = () => {
     return (
-        <div>
-            <h1>Welcome to the Main Menu</h1>
-            <p>This is the main menu for non-admin users. From here you can navigate to different parts of the application.</p>
-        </div>
+        <Container className="mt-5">
+            <Row>
+                <Col className="text-center">
+                    <h1>Main Menu</h1>
+                </Col>
+            </Row>
+
+            <Row className="mt-4">
+                <Col md={{ span: 6, offset: 3 }}>
+                    <p>
+                        Click the button below to view meetings.
+                    </p>
+                    <Link to='/meetings'>
+                        <Button block>See Meetings</Button>
+                    </Link>
+
+                    <p>
+                        Click the button below to view all complaints that have been filed
+                    </p>
+                    <Link to='/file-complaints'>
+                        <Button className="mb-3" block>File Complaint</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
