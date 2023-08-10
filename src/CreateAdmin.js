8db08e3a020a35763c7f7e3026bac442ffbd52ec
leo45rod <Leo45rodriguez@hotmail.com>
2023-08-10 18:48:26 -0400

    Create Admin finished

--- src/CreateAdmin.js
+++ src/CreateAdmin.js
@@ -1,5 +1,194 @@
+import React, { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Formik } from 'formik' // for form validation
+import * as Yup from 'yup'
+import { useHistory } from 'react-router-dom';
+import useAuth from './useAuth'
+
 const CreateAdmin = () => {
-    return ( null );
+    const { currentUser } = useAuth()
+
+    // How to validate the input on front end for registration
+    const formSchema = Yup.object({
+        email: Yup.string()
+            .min(21, 'Invalid email')
+            .max(50, 'Invalid email')
+            // pattern required for valid email
+            .matches(/^[a-zA-Z]{1,15}\.[a-zA-Z]{1,16}/,
+                'Invalid email.') // first.last
+            .matches(/@pennstatesoft.com$/, 'Must use your company email.') // must end with appropriate email domain
+            .required('Must provide an email.'),
+        name: Yup.string()
+            // Set min to allow user entering initials
+            // followed by period and space separating.
+            .min(5)
+            // Set max to a reasonable limit.
+            // Unfortunately this will not allow
+            // specifying hyphenated names and
+            // will force some long last/first
+            // name people to choose between their
+            // first and last name while using initials
+            // for their other name.
+            .max(30)
+            // Ensure only names are entered with
+            // optional period for initials
+            // and space separating first and last
+            .matches(/^[A-Z.a-z]+ [A-Z.a-z]+$/, 'Please specify your full name.')
+            .required('Must specify your full name.'),
+        password: Yup.string()
+            // Enforce reasonable minimum length for security of system.
+            .min(15)
+            // Set upper limit to prevent scripting attempts
+            // and other attacks.
+            .max(40)
+            // Specify allowable characters for password.
+            .matches(/[a-z]{1,}/, 'Must include at least 1 lowercase letter') // lowercase needed
+            .matches(/[A-Z]{1,}/, 'Must include at least 1 uppercase.') // uppercase needed
+            .matches(/[!`$%,|]{3,}/, 'Minimum 3 special characters from: !`$%,|') // special characters minimum
+            // Ensure user submits a password
+            .required('Required'),
+        confirmPassword: Yup.string() // Make sure passwords entered match
+            // Ensure user submits a password
+            .required('Please enter a password')
+            .oneOf([Yup.ref('password')], 'Passwords must match.') // Get the value of password and put it in
+        // an array.
+        // oneOf takes an array of values that this
+        // field must match against      
+    })
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const routerHistory = useHistory();
+
+    // Function to take in the form values
+    const handleSubmit = async (values) => {
+        try {
+            console.log('Form is submitting', values);
+            // Use fetch to make a POST request
+            const response = await fetch('http://localhost:5000/api/users/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: values.email,
+                    name: values.name,
+                    password: values.password,
+                    confirmPassword: values.confirmPassword,
+                    requesterIsAdmin: true, // User is set to admin by another admin
+                }),
+            });
+
+            const data = await response.json();
+
+            // If response status is not 200 it means an error
+            if (!response.ok) {
+                // Extract the JSON from error
+                throw new Error(data.message || "Failed to register");
+            }
+
+            // When this point is reached registration is successful
+            console.log("Admin created", data); // Log the response data for now
+
+            // If registration was successful, navigate to the MainMenu page
+            // Use the history object to navigate and pass a success message
+            routerHistory.push('/');
+
+        } catch (error) {
+            // Log any errors to the console
+            console.error('Failed to create admin:', error);
+        }
+    };
+
+    return (
+        <Formik
+            validationSchema={formSchema}
+            initialValues={{
+                email: '',
+                name: '',
+                password: '',
+                confirmPassword: ''
+            }}
+            onSubmit={async (values) => {
+                await handleSubmit(values)
+            }}
+        >
+            {({ handleSubmit, handleChange, values, errors }) => (
+                // To ensure padding on each side of page
+                // wrap in a container
+                // min-vh-100 ensures we use all available screen space
+
+                //d-flex: turn all inner elements into flex items
+                //flex-column: stack vertically all children
+                //align-items-center: all flex items should be centered
+                //noValidate: ensures the default HTML validation doesn't happen
+                //                this allows us to create or use our own validation
+                //               via  a 3rd party library or custom built.
+                <Form noValidate onSubmit={handleSubmit} className="min-vh-100 d-flex flex-column">
+                    <Container className='w-25'>
+                        {/* Wrap each form entry item in a Form.Group
+                                to ensure we label each form data entry
+                                and its individual accesibility features properly */}
+                        <Form.Group className='mb-2' controlId='name'> {/* Full name input */}
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="John Smith"
+                                name="name"
+                                value={values.name}
+                                onChange={handleChange}
+                                isInvalid={!!errors.name}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.name}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className='mb-2' controlId='email'> {/* Email input */}
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="aa.milne@pennstatesoft.com"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                isInvalid={!!errors.email}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.email}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className='mb-2' controlId='password'> {/* Password initial input */}
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                value={values.password}
+                                onChange={handleChange}
+                                isInvalid={!!errors.password}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.password}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId='confirmPassword'> {/* Password confirm input */}
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                name="confirmPassword"
+                                value={values.confirmPassword}
+                                onChange={handleChange}
+                                isInvalid={!!errors.confirmPassword}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.confirmPassword}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        {/* Register button */}
+                        <Button size='lg' type="submit">Create Admin</Button>
+                    </Container>
+                </Form>
+            )}
+        </Formik>
+    );
 }
- 
+
 export default CreateAdmin;
