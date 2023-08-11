a35b5ce72996570d09fa9b2e751e14d1cc94f0ae
kkeian <kaserman22@gmail.com>
2023-08-10 20:06:46 -0400

    Fix routing from Profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Container, Card, Button } from 'react-bootstrap'
 import useAuth from './useAuth';
+import { useHistory } from 'react-router-dom'
 
 const Profile = () => {
+    const history = useHistory() // to allow routing properly
     const { currentUser, loading } = useAuth();
+
+    // Handler for changing password
+    const handleChangePassword = () => {
+        history.push('/reauth')
+    }
 
     if (loading) {
         return <div>Loading...</div>;
+    }
+
+    if (currentUser === null) {
+        // then user not logged in so we need to redirect to home
+        history.push('/')
     }
 
     return (
@@ -15,7 +27,7 @@ const Profile = () => {
                 <Card.Body>
                     <Card.Title>{currentUser.name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{currentUser.email}</Card.Subtitle>
-                    <Button variant="primary" href="/reauth">
+                    <Button variant="primary" onClick={handleChangePassword}>
                         Change Password
                     </Button>
                 </Card.Body>
