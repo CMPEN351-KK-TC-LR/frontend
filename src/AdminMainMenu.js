9facbc814d22826e8f8478cf58134e94fcebe667
kkeian <kaserman22@gmail.com>
2023-08-10 20:05:45 -0400

    Create buttons and redirects for all admin functionality

--- src/AdminMainMenu.js
+++ src/AdminMainMenu.js
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom';
 import { Button, Container, Row, Col } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const AdmiMainMenu = () => {
+    // Allow redirects
+    const history = useHistory()
+
+    // redirect to correct path
+    const handleClick = (path) => {
+        history.push(path)
+    }
+
     return (
         <Container className="mt-5">
             <Row>
@@ -12,19 +21,14 @@ const AdmiMainMenu = () => {
 
             <Row className="mt-4">
                 <Col md={{ span: 6, offset: 3 }}>
-                    <p>
-                        Click the button below to view all scheduled meetings.
-                    </p>
-                    <Link to='/admin-meetings'>
-                        <Button>See Meetings</Button>
-                    </Link>
-
-                    <p>
-                        Click the button below to view all complaints that have been filed
-                    </p>
-                    <Link to='/admin-complaints'>
-                        <Button className="mb-3">View Complaints</Button>
-                    </Link>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/admin-meetings')}>See Meetings</Button>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/admin-complaints')}>View Complaints</Button>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/create-admin')}>Create Admin Account</Button>
+                </Col>
+                <Col md={{ span: 6, offset: 3 }}>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/manage-users')}>Manage Users</Button>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/manage-rooms')}>Manage Rooms</Button>
+                    <Button className="mb-3 mx-2" onClick={() => handleClick('/profile')}>Profile</Button>
                 </Col>
             </Row>
         </Container>
